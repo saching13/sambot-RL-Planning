c7342672ea622f85b451569d26af9434ca78c7c6
Sachin Guruswamy <sachin.guruswamy@gmail.com>
2020-05-06 13:10:50 -0700

    added gpu and optimization

--- DQN.py
+++ DQN.py
@@ -16,7 +16,7 @@ import random
 import tensorflow as tf
 
 
-MAX_NUM_EPISODES = 100000
+MAX_NUM_EPISODES = 10000
 NUM_TEST_EPISODES = 500
 STEPS_PER_EPISODE = 300  # This is specific to MountainCar. May change with env
 
@@ -39,7 +39,7 @@ MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
 UPDATE_TARGET_EVERY = 5  # Terminal states (end of episodes)
 # MODEL_NAME = '2x256'
 
-MEMORY_FRACTION = 0.20
+# MEMORY_FRACTION = 0.20
 MODEL_NAME = "SamBot"
 
 #  Stats settings
@@ -71,6 +71,7 @@ ACTION_SPACE_SIZE = env.action_space.n
 # ep_rewards = [-200]
 
 
+
 # For more repetitive results
 # random.seed(1)
 # np.random.seed(1)
@@ -79,42 +80,43 @@ ACTION_SPACE_SIZE = env.action_space.n
 # Agent class
 class DQNAgent:
     def __init__(self):
-
+        with tf.device('/GPU:0'):
         # Main model
-        self.model = self.create_model()
-
-        # Target network
-        self.target_model = self.create_model()
-        self.target_model.set_weights(self.model.get_weights())
-
-        # An array with last n steps for training
-        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
-
-        # Used to count when to update target network with main network's weights
-        self.target_update_counter = 0
+            self.model = self.create_model()
+    
+            # Target network
+            self.target_model = self.create_model()
+            self.target_model.set_weights(self.model.get_weights())
+    
+            # An array with last n steps for training
+            self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+    
+            # Used to count when to update target network with main network's weights
+            self.target_update_counter = 0
 
     def create_model(self):
-        model = Sequential()
-        model.add(Dense(32, kernel_initializer='random_normal', input_dim=env.observation_space.shape[0]))
-        model.add(Activation("relu"))
-        model.add(Dropout(0.2))
-
-        model.add(Dense(32, kernel_initializer='random_normal'))
-        model.add(Activation("relu"))
-        model.add(Dropout(0.2))
-
-        model.add(Dense(32, kernel_initializer='random_normal'))
-        model.add(Activation("relu"))
-        model.add(Dropout(0.2))
-
-        model.add(Dense(32, kernel_initializer='random_normal'))
-        model.add(Activation("relu"))
-        model.add(Dropout(0.2))
-
-        model.add(Dense(4, activation="linear"))
-        model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy'])
-        print(model.summary)
-        return model
+        with tf.device('/GPU:0'):
+            model = Sequential()
+            model.add(Dense(32, kernel_initializer='random_normal', input_dim=env.observation_space.shape[0]))
+            model.add(Activation("relu"))
+            model.add(Dropout(0.2))
+    
+            model.add(Dense(32, kernel_initializer='random_normal'))
+            model.add(Activation("relu"))
+            model.add(Dropout(0.2))
+    
+            model.add(Dense(32, kernel_initializer='random_normal'))
+            model.add(Activation("relu"))
+            model.add(Dropout(0.2))
+    
+            model.add(Dense(32, kernel_initializer='random_normal'))
+            model.add(Activation("relu"))
+            model.add(Dropout(0.2))
+    
+            model.add(Dense(4, activation="linear"))
+            model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy'])
+            print(model.summary)
+            return model
 
     # Adds step's data to a memory replay array
     # (observation space, action, reward, new observation space, done)
@@ -123,62 +125,66 @@ class DQNAgent:
 
     # Trains main network every step during episode
     def train(self, terminal_state, step):
-
-        # Start training only if certain number of samples is already saved
-        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
-            return
-
-        # Get a minibatch of random samples from memory replay table
-        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
-
-        # Get current states from minibatch, then query NN model for Q values
-        current_states = np.array([transition[0] for transition in minibatch])
-        current_qs_list = self.model.predict(current_states)
-
-        # Get future states from minibatch, then query NN model for Q values
-        # When using target network, query it, otherwise main network should be queried
-        new_current_states = np.array([transition[3] for transition in minibatch])
-        future_qs_list = self.target_model.predict(new_current_states)
-
-        X = []
-        y = []
-
-        # Now we need to enumerate our batches
-        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
-
-            # If not a terminal state, get new q from future states, otherwise set it to 0
-            # almost like with Q Learning, but we use just part of equation here
-            if not done:
-                max_future_q = np.max(future_qs_list[index])
-                new_q = reward + DISCOUNT * max_future_q
-            else:
-                new_q = reward
-
-            # Update Q value for given state
-            current_qs = current_qs_list[index]
-            current_qs[action] = new_q
-
-            # And append to our training data
-            X.append(current_state)
-            y.append(current_qs)
-
-        # Fit on all samples as one batch, log only on terminal state
-        self.model.fit(np.array(X), np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False)
-
-        # Update target network counter every episode
-        if terminal_state:
-            self.target_update_counter += 1
-
-        # If counter reaches set value, update target network with weights of main network
-        if self.target_update_counter > UPDATE_TARGET_EVERY:
-            self.target_model.set_weights(self.model.get_weights())
-            self.target_update_counter = 0
+        with tf.device('/GPU:0'):
+            # Start training only if certain number of samples is already saved
+            if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+                return
+    
+            # Get a minibatch of random samples from memory replay table
+            minibatch = np.asarray(random.sample(self.replay_memory, MINIBATCH_SIZE))
+    
+            # Get current states from minibatch, then query NN model for Q values
+            # current_states = np.array([transition[0] for transition in minibatch])
+            current_states = np.vstack(minibatch[:,0])
+            current_qs_list = self.model.predict(current_states)
+    
+            # Get future states from minibatch, then query NN model for Q values
+            # When using target network, query it, otherwise main network should be queried
+            # new_current_states = np.array([transition[3] for transition in minibatch])
+    
+            new_current_states = np.vstack(minibatch[:, 3])
+            future_qs_list = self.target_model.predict(new_current_states)
+    
+            X = []
+            y = []
+    
+            # Now we need to enumerate our batches
+            for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+    
+                # If not a terminal state, get new q from future states, otherwise set it to 0
+                # almost like with Q Learning, but we use just part of equation here
+                if not done:
+                    max_future_q = np.max(future_qs_list[index])
+                    new_q = reward + DISCOUNT * max_future_q
+                else:
+                    new_q = reward
+    
+                # Update Q value for given state
+                current_qs = current_qs_list[index]
+                current_qs[action] = new_q
+    
+                # And append to our training data
+                X.append(current_state)
+                y.append(current_qs)
+    
+            # Fit on all samples as one batch, log only on terminal state
+            self.model.fit(np.array(X), np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False)
+    
+            # Update target network counter every episode
+            if terminal_state:
+                self.target_update_counter += 1
+    
+            # If counter reaches set value, update target network with weights of main network
+            if self.target_update_counter > UPDATE_TARGET_EVERY:
+                self.target_model.set_weights(self.model.get_weights())
+                self.target_update_counter = 0
 
     # Queries main network for Q values given current observation space (environment state)
     def get_qs(self, state):
-        my_state = np.asarray(state)
-        return self.model.predict(np.array(my_state).reshape(-1, *my_state.shape))
-        # return self.model.predict(np.array(state).reshape(-1, *state.shape))
+        with tf.device('/GPU:0'):
+            my_state = np.asarray(state)
+            return self.model.predict(np.array(my_state).reshape(-1, *my_state.shape))
+            # return self.model.predict(np.array(state).reshape(-1, *state.shape))
 
 
 agent = DQNAgent()
@@ -192,52 +198,55 @@ def normalize_state(obs):
     for i, value in enumerate(obs):
         normalized_value = (value - obs_low[i]) / (obs_high[i] - obs_low[i])
         normalized_obs.append(normalized_value)
-    x = np.asarray(normalized_obs)
+    # x = np.asarray(normalized_obs)
     # print(x.shape)
-    return (normalized_obs)
+    return np.asarray(normalized_obs)
 
+with tf.device('/GPU:0'):
 
-for episode in tqdm(range(1, MAX_NUM_EPISODES + 1), ascii=True, unit='episodes'):
-    # Restarting episode - reset episode reward and step number
-    episode_reward = 0
-    step = 1
-
-    # Reset environment and get initial state
-    current_state = env.reset()
-    total_reward = 0.0
-    # Reset flag and start iterating until episode ends
-    done = False
-    normalized_curr_state = normalize_state(current_state)
-    while not done:
-        # This part stays mostly the same, the change is to query a model for Q values
-        if epsilon > EPSILON_MIN:
-            epsilon -= EPSILON_DECAY
-        if np.random.random() > epsilon:
-            # Get action from Q table
-            action = np.argmax(agent.get_qs(current_state))
-            # print("Action from Q-state: ",action)
-        else:
-            # Get random action
-            action = np.random.randint(0, ACTION_SPACE_SIZE)
-            # print("Action from random: ",action)
-
-        new_state, reward, done = env.step(action)
-        # Transform new continous state to new discrete state and count reward
-        episode_reward += reward
-        # Every step we update replay memory and train main network
-        normalized_new_state = normalize_state(new_state)
-
-        agent.update_replay_memory((normalized_curr_state, action, reward, normalized_new_state, done))
-        agent.train(done, step)
-        current_state = new_state
-        normalized_curr_state = normalized_new_state
-        step += 1
-        total_reward += reward
-
-    if total_reward > best_reward:
-        best_reward = total_reward
-        agent.model.save(f'models/{MODEL_NAME}__best_reward__{best_reward:_>7.2f}.model')
-    print("Episode#:{} reward:{} best_reward:{} eps:{}".format(episode,total_reward, best_reward, epsilon))
+    for episode in tqdm(range(1, MAX_NUM_EPISODES + 1), ascii=True, unit='episodes'):
+        # Restarting episode - reset episode reward and step number
+        episode_reward = 0
+        step = 1
+    
+        # Reset environment and get initial state
+        current_state = env.reset()
+        total_reward = 0.0
+        # Reset flag and start iterating until episode ends
+        done = False
+        normalized_curr_state = normalize_state(current_state)
+        while not done:
+            # This part stays mostly the same, the change is to query a model for Q values
+            if epsilon > EPSILON_MIN:
+                epsilon -= EPSILON_DECAY
+            if np.random.random() > epsilon:
+                # Get action from Q table
+                action = np.argmax(agent.get_qs(current_state))
+                # print("Action from Q-state: ",action)
+            else:
+                # Get random action
+                action = np.random.randint(0, ACTION_SPACE_SIZE)
+                # print("Action from random: ",action)
+    
+            new_state, reward, done = env.step(action)
+            # Transform new continous state to new discrete state and count reward
+            episode_reward += reward
+            # Every step we update replay memory and train main network
+            normalized_new_state = normalize_state(new_state)
+    
+            agent.update_replay_memory((normalized_curr_state, action, reward, normalized_new_state, done))
+            agent.train(done, step)
+            current_state = new_state
+            normalized_curr_state = normalized_new_state
+            step += 1
+            total_reward += reward
+    
+        if total_reward > best_reward:
+            best_reward = total_reward
+            agent.model.save(f'models/{MODEL_NAME}__best_reward__{best_reward:_>7.2f}.model')
+        print("Episode#:{} reward:{} best_reward:{} eps:{}".format(episode,total_reward, best_reward, epsilon))
+    
+    agent.target_model.save(f'models/{MODEL_NAME}_final.model')
 
     # # Decay epsilon
     # if epsilon > MIN_EPSILON:
